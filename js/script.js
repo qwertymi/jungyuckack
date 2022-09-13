25e6ca65e22eba573c66a271d61640120c52fb82
hemhem <kimhp1216@gmail.com>
2022-09-13 19:31:55 +0900

    반응형 작업중

--- js/script.js
+++ js/script.js
@@ -1,9 +1,87 @@
+/*
+  작성자 : 김혜미
+  작성일 : 2022-07-07
+  요구기능 : 
+  1. 참조사이트 : https://www.jeongyookgak.com/
+  2. 베스트 상품 데이터 출력을 위한 업무 사항을 기재하시오.
+  3. 총 데이터 항목 개수는 6개
+  4. 항목별 이름과 구조를 정의하시오.
+*/
+
+// 배열
+// 베스트 상품 보기 링크
+let itemLinkArr = [
+
+];
+// 베스트 상품 장바구니 링크
+let itemCartArr = [
+
+]
+// 베스트 상품 이미지
+let itemImgArr = [
+
+];
+// 베스트 상품 타이틀
+let itemTitleArr = [
+
+];
+// 베스트 상품 가격
+let itemPriceArr = [
+
+];
+// /배열 
+
+// 객체
+let bestItemList = [
+  {
+    link:'#',
+    cart:'#',
+    img:'1pork.png',
+    title:'초신선 돼지 삼겹살 구이용',
+    info:'기준가 23.400원/600g'
+  },
+  {
+    link:'#',
+    cart:'#',
+    img:'2chicken.png',
+    title:'초신선 닭볶음탕',
+    info:'기준가 6.700원/950g'
+  },
+  {
+    link:'#',
+    cart:'#',
+    img:'3porkcutlet.png',
+    title:'초신선 등심 돈까스',
+    info:'기준가 11.800원/770g'
+  },
+  {
+    link:'#',
+    cart:'#',
+    img:'4egg.png',
+    title:'초신선 동물복지 무항생제 유정란',
+    info:'기준가 6.900원/12구'
+  },
+  {
+    link:'#',
+    cart:'#',
+    img:'5milk.png',
+    title:'초신선 무항생제 우유',
+    info:'기준가 3.600원/900ml'
+  },
+  {
+    link:'#',
+    cart:'#',
+    img:'6hanu.png',
+    title:'초신선 무항생제 이유식용 하눙 우둔 다짐육',
+    info:'기준가 15.300원/180g'
+  }
+]
+
 // html, css, js 로딩완료
 $(document).ready();
 
 // html, css, js 멀티미디어까지 로딩완료
 window.onload = function () {
-  // visual 슬라이드
   new Swiper('.sw-visual', {
     speed: 250,
     navigation: {
